a034b35d47126432e36959f975d1224c9f96a9b7
Koen Verschuren <koenzijnpost@gmail.com>
2015-06-17 10:31:01 +0200

    Deleted virtual env and created initial skill matrix interface

--- js/controllers/skillmatrixController.js
+++ js/controllers/skillmatrixController.js
@@ -1,4 +1,15 @@
 appControllers.controller('skillmatrixCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    console.log("CHECK")
+	$scope.skillLevels = ["none", "weak", "average", "strong", "expert"]
+    $http.get('http://bdr-skillmatrix-api-dev.elasticbeanstalk.com/skills').success(function(data) {
+    	$scope.skillsByGroup = data;
+    })
+    
+    $scope.userID = 6040530933;
+	
+	$scope.scoreSkill = function(skill, score) {
+		$http.post('http://bdr-skillmatrix-api-dev.elasticbeanstalk.com/skills').success(function(result) {
+			console.log(result)
+	    })
+	}
   }]);
